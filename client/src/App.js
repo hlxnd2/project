926bb3ced05c65c4637cd5e51e00a4c92e456f02
Calvo <julio.calvo@itu.int>
2022-05-06 14:34:52 +0200

    New structure and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
-function App() {
+import './App.css';
+import ClotheList from './components/ClotheList/ClotheList';
+import { useClothes } from './hooks/useClothes';
+
+//Temp stuff
+import { data } from './data';
+
+export default function App() {
+  const [clothes, setClothes] = useState([
+    {
+      id: 1,
+      title: 'Shorts',
+      category: 'Sport',
+      description: 'So confortable',
+      size: 'M',
+      color: 'Orange',
+      img_src:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Forange-men-shorts-for-swimming-picture-id697913308%3Fk%3D20%26m%3D697913308%26s%3D612x612%26w%3D0%26h%3Dl2x4DyjDZmwEcekPZDRp1b5ImMGAUSxTMFZ8GGhISZ8%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fde%2Ffotos%2Fshorts&tbnid=1UgKmHxUh_IldM&vet=12ahUKEwjguNHA7Mr3AhXKKewKHc_5As4QMygAegUIARC9Ag..i&docid=Isfnm73cCQlwiM&w=612&h=408&q=image%20shorts&safe=active&ved=2ahUKEwjguNHA7Mr3AhXKKewKHc_5As4QMygAegUIARC9Ag',
+    },
+
+    {
+      id: 2,
+      title: 'T-shirt',
+      category: 'Casual',
+      description: '100% cotton',
+      size: 'L',
+      color: 'Pink',
+      img_src:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F2530%2F3930%2Fproducts%2FPink.jpg%3Fv%3D1584446024&imgrefurl=https%3A%2F%2Fcommunityclothing.co.uk%2Fproducts%2Fwomensshortsleevet-shirt-pink&tbnid=QDebqotM_66ufM&vet=12ahUKEwjJ9ebt7Mr3AhXQjqQKHWzYBLIQMygMegUIARDRAg..i&docid=hJz01t5FH2KwbM&w=880&h=880&q=t-shirt%20pink&hl=es&safe=active&ved=2ahUKEwjJ9ebt7Mr3AhXQjqQKHWzYBLIQMygMegUIARDRAg',
+    },
+  ]);
+  //{data.clothes.map((clothe, key) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3 className='p-3 text-center'>React - Display a list of items</h3>
+      <table className='table table-striped table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clothes &&
+            clothes.map((clothe) => (
+              <tr key={clothe.id}>
+                <td>{clothe.title}</td>
+                <td>{clothe.category}</td>
+                <td>{clothe.description}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
---
+++ client/src/services/ApiClient.js
@@ -0,0 +1,28 @@
+import Clothe from "../models/Clothe";
+
+export function fetchShow() {
+  return fetch('')
+    .then(response => response.json())
+    .then(Clothing => Clothing.map(Clothing.create))
+}
+
+export const data = {
+  clothes: [
+    {
+      title: 'Shorts',
+      category: 'Sport',
+      description: 'So confortable',
+      size: 'M',
+      color: 'Orange',
+      img_src: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Forange-men-shorts-for-swimming-picture-id697913308%3Fk%3D20%26m%3D697913308%26s%3D612x612%26w%3D0%26h%3Dl2x4DyjDZmwEcekPZDRp1b5ImMGAUSxTMFZ8GGhISZ8%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fde%2Ffotos%2Fshorts&tbnid=1UgKmHxUh_IldM&vet=12ahUKEwjguNHA7Mr3AhXKKewKHc_5As4QMygAegUIARC9Ag..i&docid=Isfnm73cCQlwiM&w=612&h=408&q=image%20shorts&safe=active&ved=2ahUKEwjguNHA7Mr3AhXKKewKHc_5As4QMygAegUIARC9Ag'
+    },
+    {
+      title: 'T-shirt',
+      category: 'Casual',
+      description: '100% cotton',
+      size: 'L',
+      color: 'Pink',
+      img_src: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F2530%2F3930%2Fproducts%2FPink.jpg%3Fv%3D1584446024&imgrefurl=https%3A%2F%2Fcommunityclothing.co.uk%2Fproducts%2Fwomensshortsleevet-shirt-pink&tbnid=QDebqotM_66ufM&vet=12ahUKEwjJ9ebt7Mr3AhXQjqQKHWzYBLIQMygMegUIARDRAg..i&docid=hJz01t5FH2KwbM&w=880&h=880&q=t-shirt%20pink&hl=es&safe=active&ved=2ahUKEwjJ9ebt7Mr3AhXQjqQKHWzYBLIQMygMegUIARDRAg'
+    }
+  ]
+};
