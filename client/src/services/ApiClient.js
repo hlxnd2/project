52835d8b1c0b726c55b7ec356bb958ef782f7cc5
Rémi Ducceschi <remi.ducceschi@itu.int>
2022-05-10 15:03:35 +0200

    post cart

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -6,3 +6,14 @@ export function fetchProducts() {
     .then((products) => products.map(Product.create));
 }
 
+export function createOrder(products, total, discount) {
+    return fetch('http://localhost:3030/orders', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({products, total, discount})
+    })
+    .then((response) => response.json());
+}
